020e3aafa63a70334655ba74382224b8ff4aab13
Kevin Anthony da Cruz Riquena <riquena969@gmail.com>
2017-12-23 22:46:22 -0200

    Adicionado parte de Habilidades

--- libs/custom/script.js
+++ libs/custom/script.js
@@ -5,4 +5,23 @@ $(document).ready(function() {
     });
 
     new WOW().init();
+    inserirEstrelas();
 });
+
+function inserirEstrelas() {
+    $('.estrelas').each(function(key, element) {
+        let quantidadeEstrelas = $(element).attr('quantidadeEstrelas');
+        let htmlEstrelas       = '';
+
+        for (var i = 0; i < quantidadeEstrelas; i++) {
+            htmlEstrelas += '<span class="glyphicon glyphicon-star" aria-hidden="true"></span>';
+        }
+        for (var i = 0; i < (5 - quantidadeEstrelas); i++) {
+            htmlEstrelas += '<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>';
+        }
+
+        $(element).html(htmlEstrelas);
+        console.log(element);
+        console.log(htmlEstrelas);
+    });
+}
